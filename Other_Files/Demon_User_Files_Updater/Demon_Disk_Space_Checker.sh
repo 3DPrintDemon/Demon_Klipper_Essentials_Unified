069cf7dce5cf99ea9e69926115da2d08f148e898
3DPrintDemon <122202359+3DPrintDemon@users.noreply.github.com>
2025-01-01 18:47:51 +0100

    Update Demon_Disk_Space_Checker.sh

--- Other_Files/Demon_User_Files_Updater/Demon_Disk_Space_Checker.sh
+++ Other_Files/Demon_User_Files_Updater/Demon_Disk_Space_Checker.sh
@@ -3,10 +3,10 @@
 # Define
 SPACE=`df -k --output=avail "$PWD" | tail -n1`
  
-if [ $SPACE -lt 104850 ]; then
+if [ $SPACE -lt 524250 ]; then
     df -h / --output=source,size,used,avail,pcent
     echo "_LOW_SPACE_PROMPT" >~/printer_data/comms/klippy.serial
-    echo "Disk Space Check: WARNING less than 100MBs free!"
+    echo "Disk Space Check: WARNING less than 500MBs free!"
       
 else
     df -h / --output=source,size,used,avail,pcent
