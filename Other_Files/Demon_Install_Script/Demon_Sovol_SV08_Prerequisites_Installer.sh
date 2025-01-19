9f7da008fc7db24fd74d18a51a33746dab679c13
3DPrintDemon <122202359+3DPrintDemon@users.noreply.github.com>
2025-01-19 16:53:58 +0100

    Update Demon_Sovol_SV08_Prerequisites_Installer.sh

--- Other_Files/Demon_Install_Script/Demon_Sovol_SV08_Prerequisites_Installer.sh
+++ Other_Files/Demon_Install_Script/Demon_Sovol_SV08_Prerequisites_Installer.sh
@@ -46,42 +46,5 @@ else
 fi
 
 
-# prompting
-read -p "Do you want to install the Demon Sovol SV08 Stock Display menu system? Recommended! - (y)Yes/(n)No:  " choice
 
-# action
-case $choice in
-[yY]* ) echo "Attempting SV08 Demon Stock Display Config installation!"
-    if [ -d "$DDIR" ]; then
-        cd $DDIR
-        echo "display.cfg backup created inside directory $DDIR"
-        wget https://github.com/3DPrintDemon/SV08/releases/download/v1.0/display.cfg --backups=1
-        echo "menu.cfg backup created inside directory $DDIR"
-        wget https://github.com/3DPrintDemon/SV08/releases/download/v1.0/menu.cfg --backups=1
-        cd $CDIR
-        echo "Demon SV08 Stock Display Config installer actions complete"
-        echo "Install complete! Please restat Klipper to use your new menu system."
-    else
-        if [ -d "$UFDIR" ]; then
-            if [ ! -f "$UFDIR/demon_SV08_ML_display_v1.0.cfg" ]; then
-                echo "Mainline system detected, installing Demon_SV08_Mainline_v1.0.cfg in directory $UFDIR"
-                cd $UFDIR 
-                wget https://github.com/3DPrintDemon/SV08/releases/download/v1.0/demon_SV08_ML_display_v1.0.cfg --backups=1
-                cd $CDIR
-                echo "Demon SV08 Stock Display Config installer actions complete"
-                echo "Install complete! Please restart Klipper to use your new menu system."
-        
-            else
-                echo "demon_SV08_ML_display_v1.0.cfg file already exists."
-            fi
-            
-        else
-            echo "$UFDIR does not exist, please check your system or install the main macro files first, demon_SV08_ML_display_v1.0.cfg cancelled!"
-        fi
-    fi
-    echo "Installer actions complete." ;;
-[nN]* ) echo "Installer actions complete" ;;
-*) exit ;;
-esac
-
-echo "End of line."
+echo "Installer actions complete"
