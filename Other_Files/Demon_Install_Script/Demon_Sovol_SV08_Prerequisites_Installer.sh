b594c35c86125821fcc3fb03f090c7ec27c744d0
3DPrintDemon <122202359+3DPrintDemon@users.noreply.github.com>
2025-01-19 19:02:48 +0100

    Update Demon_Sovol_SV08_Prerequisites_Installer.sh

--- Other_Files/Demon_Install_Script/Demon_Sovol_SV08_Prerequisites_Installer.sh
+++ Other_Files/Demon_Install_Script/Demon_Sovol_SV08_Prerequisites_Installer.sh
@@ -45,6 +45,58 @@ else
     echo "Heat_Soak_Sovol_SV08.cfg file already exists. Skipping install."
 fi
 
+# prompting
+read -p "Do you want to install the Demon Sovol SV08 Stock Display menu system? Recommended! - (y)Yes/(n)No:  " choice
 
+# action
+case $choice in
+[yY]* ) echo "Attempting SV08 Demon Stock Display Config installation!"
+    
+    # prompting
+    read -p "Are you Running a stock Sovol system or modified Mainline Klipper system - (s)Stock/(m)Mainline:  " choice
+    
+    # action
+    case $choice in
+    [sS]* ) echo "Ok Sovol stock system it is!"
+        if [ -d "$DDIR" ]; then
+            cd $DDIR
+            echo "display.cfg backup created inside directory $DDIR"
+            wget https://github.com/3DPrintDemon/SV08/releases/download/v1.0/display.cfg --backups=1
+            echo "menu.cfg backup created inside directory $DDIR"
+            wget https://github.com/3DPrintDemon/SV08/releases/download/v1.0/menu.cfg --backups=1
+            cd $CDIR
+            echo "Demon SV08 Stock Display Config installer actions complete"
+            echo "Install complete! Please restart Klipper to use your new menu system."
+
+        else
+            echo "$DDIR does not exist, cancelling this install!"
+        fi ;;
+    
+    [mM]* ) echo "Ok modified Mainline Klipper system it is!"
+        if [ -d "$UFDIR" ]; then
+            if [ ! -f "$UFDIR/demon_SV08_ML_display_v1.0.cfg" ]; then
+                echo "Installing Demon_SV08_Mainline_v1.0.cfg in directory $UFDIR"
+                cd $UFDIR 
+                wget https://github.com/3DPrintDemon/SV08/releases/download/v1.0/demon_SV08_ML_display_v1.0.cfg --backups=1
+                cd $CDIR
+                echo "Demon SV08 Mainline Display Config installer actions complete"
+                echo "Install complete! Please restart Klipper to use your new menu system."
+        
+            else
+                echo "demon_SV08_ML_display_v1.0.cfg file already exists."
+            fi
+            
+        else
+            echo "$UFDIR does not exist, please check your system or install the main macro files first, demon_SV08_ML_display_v1.0.cfg install cancelled!"
+        fi ;;
+
+    *) exit ;;
+    esac    
+    ;;
+    
+[nN]* ) echo "Not installing menu files."
+;;
+*) exit ;;
+esac
 
 echo "Installer actions complete"
