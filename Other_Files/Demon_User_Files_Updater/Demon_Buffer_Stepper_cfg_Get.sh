5f37944b995013cc859791661d254ea359486d2a
3DPrintDemon <122202359+3DPrintDemon@users.noreply.github.com>
2025-08-28 00:57:51 +0200

    Add files via upload

---
+++ Other_Files/Demon_User_Files_Updater/Demon_Buffer_Stepper_cfg_Get.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ~/printer_data/config
+wget https://raw.githubusercontent.com/3DPrintDemon/DEMON-SV08MAX/refs/heads/main/DEMON_SV08_MAX_REPLACEMENT_FILES/buffer_stepper.cfg  --backups=1
