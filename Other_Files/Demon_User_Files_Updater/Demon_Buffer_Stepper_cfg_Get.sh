3aeda6a713f3c9230e054b3d62a43bbefea0f28f
3DPrintDemon <122202359+3DPrintDemon@users.noreply.github.com>
2025-09-16 22:20:15 +0200

    Delete Other_Files/Demon_User_Files_Updater/Demon_Buffer_Stepper_cfg_Get.sh

--- Other_Files/Demon_User_Files_Updater/Demon_Buffer_Stepper_cfg_Get.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ~/printer_data/config
-wget https://raw.githubusercontent.com/3DPrintDemon/DEMON-SV08MAX/refs/heads/main/DEMON_SV08_MAX_REPLACEMENT_FILES/buffer_stepper.cfg  --backups=1
