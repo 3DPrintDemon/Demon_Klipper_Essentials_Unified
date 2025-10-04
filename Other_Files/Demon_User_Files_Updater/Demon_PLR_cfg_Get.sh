5b709c47de78385ef0420074c94a60ac4f774ab7
3DPrintDemon <122202359+3DPrintDemon@users.noreply.github.com>
2025-10-04 10:08:36 +0200

    Update Demon_PLR_cfg_Get.sh

--- Other_Files/Demon_User_Files_Updater/Demon_PLR_cfg_Get.sh
+++ Other_Files/Demon_User_Files_Updater/Demon_PLR_cfg_Get.sh
@@ -3,5 +3,5 @@
 cd ~/printer_data/config
 wget https://raw.githubusercontent.com/3DPrintDemon/DEMON-SV08MAX/refs/heads/main/DEMON_SV08_MAX_REPLACEMENT_FILES/plr.cfg --backups=1
 
-cd ~/
+cd ~/home/sovol
 wget https://raw.githubusercontent.com/3DPrintDemon/DEMON-SV08MAX/refs/heads/main/DEMON_SV08_MAX_REPLACEMENT_FILES/plr.sh --backups=1
