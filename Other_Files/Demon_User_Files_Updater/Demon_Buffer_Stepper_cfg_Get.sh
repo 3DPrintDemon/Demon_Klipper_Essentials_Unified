07f07d2c7647a6f91f13215b0a6327d5e91d696e
3DPrintDemon <122202359+3DPrintDemon@users.noreply.github.com>
2026-02-22 12:58:45 +0100

    Delete Other_Files directory

--- Other_Files/Demon_User_Files_Updater/Demon_Buffer_Stepper_cfg_Get.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ~/printer_data/config
-wget https://raw.githubusercontent.com/3DPrintDemon/DEMON-SV08MAX/refs/heads/main/DEMON_SV08_MAX_REPLACEMENT_FILES/buffer_stepper.cfg  --backups=1
-
-cd ~/klipper/klippy/extras
-wget https://raw.githubusercontent.com/3DPrintDemon/DEMON-SV08MAX/refs/heads/main/DEMON_SV08_MAX_REPLACEMENT_FILES/filament_switch_sensor.py --backups=1
-wget https://raw.githubusercontent.com/3DPrintDemon/DEMON-SV08MAX/refs/heads/main/DEMON_SV08_MAX_REPLACEMENT_FILES/buffer_stepper.py --backups=1
