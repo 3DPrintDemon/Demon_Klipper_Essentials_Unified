c26510e61b9234801443456da1940584fd7e8ee0
3DPrintDemon <122202359+3DPrintDemon@users.noreply.github.com>
2026-02-26 12:16:01 +0100

    Update Demon_User_Filament.sh

--- Other_Files/Demon_User_Files_Updater/Demon_User_Filament.sh
+++ Other_Files/Demon_User_Files_Updater/Demon_User_Filament.sh
@@ -42,4 +42,14 @@
 
  cp /home/$U/printer_data/config/Demon_Klipper_Essentials_Unified/Other_Files/DEMON_User_Files_SOURCE/demon_user_settings_filament_variables_v*.cfg /home/$U/printer_data/config/Demon_User_Files --backup=numbered
 
+ wait
+
+ echo "Requesting Klipper to set SAVE_VARIABLE VARIABLE=updated_filament VALUE=True"
+ echo "SAVE_VARIABLE VARIABLE=updated_filament VALUE=True" >~/printer_data/comms/klippy.serial
+
+ wait
+
+ echo "Requesting Klipper to RESTART"
+ echo "RESTART" >~/printer_data/comms/klippy.serial
+ 
  echo "Operations complete."
