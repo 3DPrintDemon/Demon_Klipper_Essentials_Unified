5bb4939e2384e24db2fdb4e15d48f0e5be8316e0
3DPrintDemon <122202359+3DPrintDemon@users.noreply.github.com>
2026-02-28 12:06:14 +0100

    Add files via upload

---
+++ Other_Files/Demon_Themes/Demon_Themes_Installers/Demon_Sovol_Theme_Modern_Installer.sh
@@ -0,0 +1,55 @@
+
+
+#!/bin/bash
+
+# 3DPrintDemon Demon Theme Modern Installer Script v1.0.1
+
+# Define
+U="$USER"
+TDIR="/home/$U/printer_data/config/.theme"
+PDIR="/home/$U/printer_data/config/Previous_Themes"
+SDIR="/home/$U/printer_data/config/Demon_Klipper_Essentials_Unified/Other_Files/Demon_Themes/Demon_Sovol_Theme_Modern"
+
+# Check if directory not exist
+if [ ! -d "$TDIR" ]; then
+    echo "$TDIR does not exist."
+
+    # Make Directory
+    mkdir "$TDIR"
+    echo "Directory $TDIR created."
+    wait
+
+else
+    echo "Directory $TDIR already exists."
+
+    if [ "$(ls -A $TDIR)" ]; then
+        echo "Directory $TDIR is not empty."
+
+        if [ ! -f "$TDIR/DTI.txt" ]; then
+            echo "A non-DEMON theme was detected! Moving files for you!"
+            if [ ! -d "$PDIR" ]; then
+                # Make Directory
+                mkdir "$PDIR"
+                echo "Directory $PDIR created."
+
+            else
+                echo "Directory $PDIR already exists."
+                
+            fi
+
+            echo "Moving old theme files to $PDIR"   
+            mv $TDIR/* $PDIR --backup=numbered
+
+        else
+            echo "A DEMON theme was detected! Replacing files!"
+            rm $TDIR/*
+            
+        fi
+    fi
+fi
+
+wait
+cp  $SDIR/* $TDIR  
+echo "Demon Sovol Theme Modern installed!"
+
+echo "Operations complete."
